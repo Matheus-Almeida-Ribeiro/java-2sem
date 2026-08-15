2a70ca6ad438de6c55b99bfcda8a2a0b35fa09f1
Matheus-Almeida-Ribeiro <matheus.a.ribeiro0505@gmail.com>
2026-08-14 21:35:40 -0300

    adiciona descrição e gêneros e implementa o formulario de cadastro de livros

--- src/main/java/com/com/fiap/springmvc/model/Genero.java
+++ src/main/java/com/com/fiap/springmvc/model/Genero.java
@@ -1,15 +1,26 @@
 package com.com.fiap.springmvc.model;
 
 public enum Genero {
-    TERROR,
-    ROMANCE,
-    ACAO,
-    SUSPENCE,
-    FICCAO_CIENTIFICA,
-    BIOGRAFIA,
-    FILOSOFIA,
-    RELIGIOSO,
-    HISTORIA,
-    CIENCIAS
+    TERROR("Terror"),
+    ROMANCE("Romance"),
+    ACAO("Ação"),
+    SUSPENCE("Suspence"),
+    FICCAO_CIENTIFICA("Ficção Científica"),
+    BIOGRAFIA("Biografia"),
+    FILOSOFIA("Filosofia"),
+    RELIGIOSO("Religioso"),
+    HISTORIA("Historia"),
+    CIENCIAS("Ciencia"),
+    DIDATICO("Didatico");
+
+    private final String descricao;
+
+    Genero(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 
 }
